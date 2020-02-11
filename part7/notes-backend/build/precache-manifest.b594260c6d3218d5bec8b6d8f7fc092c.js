c2a724d011fd8fd6651eeb56a2444d23a1a9a389
binhnefits <ebinhpham@gmail.com>
2020-02-10 22:04:58 -0800

    init for part7

---
+++ part7/notes-backend/build/precache-manifest.b594260c6d3218d5bec8b6d8f7fc092c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a629b3874dd8cf1c92ae2464f715ea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fba288d2163425be3ed",
+    "url": "/static/css/main.5f6b43d4.chunk.css"
+  },
+  {
+    "revision": "ba2ea589b7f882de5e34",
+    "url": "/static/js/2.7c91b131.chunk.js"
+  },
+  {
+    "revision": "7fba288d2163425be3ed",
+    "url": "/static/js/main.e02b5252.chunk.js"
+  },
+  {
+    "revision": "6205f4e2798ca22e3d86",
+    "url": "/static/js/runtime~main.4186a107.js"
+  }
+]);
